9fd79ce6f8f57ca54c92e361e649f879837f6c99
fucku738386U <wlnrxbk1789+yhxko@outlook.com>
2026-08-02 19:04:26 +0530

    Add Application class

---
+++ app/src/main/java/com/anonymous/drcconnector/DrcApp.kt
@@ -0,0 +1,10 @@
+package com.anonymous.drcconnector
+
+import android.app.Application
+
+class DrcApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Initialize app-level components here
+    }
+}
